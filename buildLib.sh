364eff6edc3b4adf642ea144d8ec5c55f98c189f
Martín Dias <tinchodias@gmail.com>
2024-02-06 20:57:42 -0300

    add build script for linx and mac

---
+++ buildLib.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -xe
+
+COMMIT_HASH=d0239ef4599b3688a5a336373f7d0a68426414ba
+
+rm -rf build
+mkdir build
+cd build
+
+wget https://github.com/slembcke/Chipmunk2D/archive/$COMMIT_HASH.zip
+unzip $COMMIT_HASH.zip 
+cd Chipmunk2D-$COMMIT_HASH/
+
+cmake .
+make
+
+cd ../../
+THE_LIBS=build/Chipmunk2D-$COMMIT_HASH/src/libchipmunk.so*
+mv $THE_LIBS .
+rm -rf build
+
